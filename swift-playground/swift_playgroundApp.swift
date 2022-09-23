414e2d2185cf57f434b9853cb46102c711be24d2
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2022-09-23 21:50:21 +0300

    Hello world

---
+++ swift-playground/swift_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_playgroundApp.swift
+//  swift-playground
+//
+//  Created by Gökhan Yılmaz on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct swift_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
